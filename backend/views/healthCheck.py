dd778bd507f373fa539a278aeac57a14c83eada7
Akash Rathor <akash.rathor@capriglobal.in>
2024-07-09 16:55:14 +0530

    minor changes

--- backend/views/healthCheck.py
+++ backend/views/healthCheck.py
@@ -6,5 +6,9 @@ from backend.utils.datetime import datetime
 
 @api_view(["GET"])
 def health_check(request):
-    data = {"status": "OK", "current date and time": datetime.get_date_time()}
+    data = {
+        "status": "OK", 
+        "current date and time": datetime.get_date_time(),
+        "current date and time": datetime.get_date_time()
+        }
     return Response(data=data, status=status.HTTP_200_OK)
